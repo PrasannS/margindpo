c16f37824d0fe1d329bf2c08b5cd52705016423b
Prasann Singhal <prasannsinghal.dev@gmail.com>
2024-07-04 00:33:02 -0500

    initial margin dpo monkey patch code

---
+++ margin_dpo.py
@@ -0,0 +1,316 @@
+# file to override TRL's normal DPO behavior with margin loss stuff
+import trl.trainer.dpo_trainer as dpo_trainer
+from typing import Dict, Union, List, Literal, Tuple, Optional
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from transformers import PreTrainedModel, AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+
+def tokenize_row(self, feature, model: Optional[Union[PreTrainedModel, nn.Module]] = None) -> Dict:
+    """Tokenize a single row from a DPO specific dataset.
+
+    At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
+    in case the prompt + chosen or prompt + rejected responses is/are too long. First
+        we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+
+    We also create the labels for the chosen/rejected responses, which are of length equal to
+        the sum of the length of the prompt and the chosen/rejected response, with
+        label_pad_token_id  for the prompt tokens.
+    """
+    batch = {}
+    prompt = feature["prompt"]
+    chosen = feature["chosen"]
+    rejected = feature["rejected"]
+
+    if not self.is_encoder_decoder:
+        # Check issues below for more details
+        #  1. https://github.com/huggingface/trl/issues/907
+        #  2. https://github.com/EleutherAI/lm-evaluation-harness/pull/531#issuecomment-1595586257
+        #  3. https://github.com/LianjiaTech/BELLE/issues/337
+
+        if not isinstance(prompt, str):
+            raise ValueError(f"prompt should be an str but got {type(prompt)}")
+        prompt_tokens = self.tokenizer(prompt, add_special_tokens=False)
+        prompt_tokens = {f"prompt_{k}": v for k, v in prompt_tokens.items()}
+
+        if not isinstance(chosen, str):
+            raise ValueError(f"chosen should be an str but got {type(chosen)}")
+        chosen_tokens = self.build_tokenized_answer(prompt, chosen)
+
+        if not isinstance(rejected, str):
+            raise ValueError(f"rejected should be an str but got {type(rejected)}")
+        rejected_tokens = self.build_tokenized_answer(prompt, rejected)
+
+        # Last prompt token might get merged by tokenizer and
+        # it should not be included for generation if that happens
+        prompt_len_input_ids = len(prompt_tokens["prompt_input_ids"])
+
+        chosen_prompt_len_input_ids = len(chosen_tokens["prompt_input_ids"])
+        rejected_prompt_len_input_ids = len(rejected_tokens["prompt_input_ids"])
+        prompt_len_input_ids = min(chosen_prompt_len_input_ids, rejected_prompt_len_input_ids)
+
+        for k, v in prompt_tokens.items():
+            prompt_tokens[k] = v[:prompt_len_input_ids]
+
+        # Make sure prompts only have one different token at most an
+        # and length only differs by 1 at most
+        num_diff_tokens = sum(
+            [a != b for a, b in zip(chosen_tokens["prompt_input_ids"], rejected_tokens["prompt_input_ids"])]
+        )
+        num_diff_len = abs(chosen_prompt_len_input_ids - rejected_prompt_len_input_ids)
+        if num_diff_tokens > 1 or num_diff_len > 1:
+            raise ValueError(
+                "Chosen and rejected prompt_input_ids might only differ on the "
+                "last token due to tokenizer merge ops."
+            )
+
+        # add BOS token to head of prompt. Avoid adding if it's already there
+        bos_token_id = self.tokenizer.bos_token_id
+        if prompt_len_input_ids == 0 or bos_token_id != prompt_tokens["prompt_input_ids"][0]:
+            prompt_tokens["prompt_input_ids"] = [bos_token_id] + prompt_tokens["prompt_input_ids"]
+            prompt_tokens["prompt_attention_mask"] = [1] + prompt_tokens["prompt_attention_mask"]
+        if chosen_prompt_len_input_ids == 0 or bos_token_id != chosen_tokens["prompt_input_ids"][0]:
+            chosen_tokens["prompt_input_ids"] = [bos_token_id] + chosen_tokens["prompt_input_ids"]
+            chosen_tokens["prompt_attention_mask"] = [1] + chosen_tokens["prompt_attention_mask"]
+        if rejected_prompt_len_input_ids == 0 or bos_token_id != rejected_tokens["prompt_input_ids"][0]:
+            rejected_tokens["prompt_input_ids"] = [bos_token_id] + rejected_tokens["prompt_input_ids"]
+            rejected_tokens["prompt_attention_mask"] = [1] + rejected_tokens["prompt_attention_mask"]
+
+        # add EOS token to end of answer. Avoid adding if it's already there
+        eos_token_id = self.tokenizer.eos_token_id
+        if len(chosen_tokens["input_ids"]) == 0 or eos_token_id != chosen_tokens["input_ids"][-1]:
+            chosen_tokens["input_ids"].append(eos_token_id)
+            chosen_tokens["attention_mask"].append(1)
+        if len(rejected_tokens["input_ids"]) == 0 or eos_token_id != rejected_tokens["input_ids"][-1]:
+            rejected_tokens["input_ids"].append(eos_token_id)
+            rejected_tokens["attention_mask"].append(1)
+
+        longer_response_length = max(len(chosen_tokens["input_ids"]), len(rejected_tokens["input_ids"]))
+
+        # if combined sequence is too long, truncate the prompt
+        for answer_tokens in [chosen_tokens, rejected_tokens, prompt_tokens]:
+            if len(answer_tokens["prompt_input_ids"]) + longer_response_length > self.max_length:
+                if self.truncation_mode == "keep_start":
+                    for k in ["prompt_input_ids", "prompt_attention_mask"]:
+                        answer_tokens[k] = answer_tokens[k][: self.max_prompt_length]
+                elif self.truncation_mode == "keep_end":
+                    for k in ["prompt_input_ids", "prompt_attention_mask"]:
+                        answer_tokens[k] = answer_tokens[k][-self.max_prompt_length :]
+                else:
+                    raise ValueError(f"Unknown truncation mode: {self.truncation_mode}")
+
+        # if that's still too long, truncate the response
+        for answer_tokens in [chosen_tokens, rejected_tokens]:
+            if len(answer_tokens["prompt_input_ids"]) + longer_response_length > self.max_length:
+                for k in ["input_ids", "attention_mask"]:
+                    answer_tokens[k] = answer_tokens[k][: self.max_length - self.max_prompt_length]
+
+        # Create labels
+        chosen_sequence_tokens = {
+            k: chosen_tokens[f"prompt_{k}"] + chosen_tokens[k] for k in ["input_ids", "attention_mask"]
+        }
+        rejected_sequence_tokens = {
+            k: rejected_tokens[f"prompt_{k}"] + rejected_tokens[k] for k in ["input_ids", "attention_mask"]
+        }
+        chosen_sequence_tokens["labels"] = chosen_sequence_tokens["input_ids"][:]
+        chosen_sequence_tokens["labels"][: len(chosen_tokens["prompt_input_ids"])] = [
+            self.label_pad_token_id
+        ] * len(chosen_tokens["prompt_input_ids"])
+        rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
+        rejected_sequence_tokens["labels"][: len(rejected_tokens["prompt_input_ids"])] = [
+            self.label_pad_token_id
+        ] * len(rejected_tokens["prompt_input_ids"])
+
+        for k, toks in {
+            "chosen_": chosen_sequence_tokens,
+            "rejected_": rejected_sequence_tokens,
+            "": prompt_tokens,
+        }.items():
+            for type_key, tokens in toks.items():
+                if type_key == "token_type_ids":
+                    continue
+                batch[f"{k}{type_key}"] = tokens
+
+    else:
+        chosen_tokens = self.tokenizer(
+            chosen, truncation=True, max_length=self.max_target_length, add_special_tokens=True
+        )
+        rejected_tokens = self.tokenizer(
+            rejected, truncation=True, max_length=self.max_target_length, add_special_tokens=True
+        )
+        prompt_tokens = self.tokenizer(
+            prompt, truncation=True, max_length=self.max_prompt_length, add_special_tokens=True
+        )
+
+        batch["chosen_labels"] = chosen_tokens["input_ids"]
+        batch["rejected_labels"] = rejected_tokens["input_ids"]
+        batch["prompt_input_ids"] = prompt_tokens["input_ids"]
+        batch["prompt_attention_mask"] = prompt_tokens["attention_mask"]
+
+        if model is not None and hasattr(model, "prepare_decoder_input_ids_from_labels"):
+            batch["rejected_decoder_input_ids"] = model.prepare_decoder_input_ids_from_labels(
+                labels=torch.tensor(batch["rejected_labels"])
+            )
+            batch["chosen_decoder_input_ids"] = model.prepare_decoder_input_ids_from_labels(
+                labels=torch.tensor(batch["chosen_labels"])
+            )
+    batch['margins'] = feature['margins']
+
+    return batch
+
+def margin_dpo_loss(
+        self,
+        policy_chosen_logps: torch.FloatTensor,
+        policy_rejected_logps: torch.FloatTensor,
+        reference_chosen_logps: torch.FloatTensor,
+        reference_rejected_logps: torch.FloatTensor,
+        batch: Dict[str, Union[List, torch.LongTensor]],
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
+
+        pi_logratios = policy_chosen_logps - policy_rejected_logps
+        if self.reference_free:
+            ref_logratios = torch.tensor([0], dtype=pi_logratios.dtype, device=pi_logratios.device)
+        else:
+            ref_logratios = reference_chosen_logps - reference_rejected_logps
+
+        pi_logratios = pi_logratios.to(self.accelerator.device)
+        ref_logratios = ref_logratios.to(self.accelerator.device)
+        logits = pi_logratios - ref_logratios
+
+        # The beta is a temperature parameter for the DPO loss, typically something in the range of 0.1 to 0.5.
+        # We ignore the reference model as beta -> 0. The label_smoothing parameter encodes our uncertainty about the labels and
+        # calculates a conservative DPO loss.
+        if self.loss_type == "sigmoid":
+            losses = (
+                -F.logsigmoid(self.beta * logits - batch['margins']) * (1 - self.label_smoothing)
+                - F.logsigmoid(-self.beta * logits - batch['margins']) * self.label_smoothing
+            )
+        elif self.loss_type == "robust":
+            losses = (
+                -F.logsigmoid(self.beta * logits - batch['margins']) * (1 - self.label_smoothing)
+                + F.logsigmoid(-self.beta * logits - batch['margins']) * self.label_smoothing
+            ) / (1 - 2 * self.label_smoothing)
+        elif self.loss_type == "ipo":
+            # eqn (17) of the paper where beta is the regularization parameter for the IPO loss, denoted by tau in the paper.
+            losses = (batch['margins'] - logits - 1 / (2 * self.beta)) ** 2
+
+        chosen_rewards = (
+            self.beta
+            * (
+                policy_chosen_logps.to(self.accelerator.device) - reference_chosen_logps.to(self.accelerator.device)
+            ).detach()
+        )
+        rejected_rewards = (
+            self.beta
+            * (
+                policy_rejected_logps.to(self.accelerator.device)
+                - reference_rejected_logps.to(self.accelerator.device)
+            ).detach()
+        )
+
+        return losses, chosen_rewards, rejected_rewards
+
+    
+def margin_loss_metrics(
+        self,
+        model,
+        batch: Dict[str, Union[List, torch.LongTensor]],
+        train_eval: Literal["train", "eval"] = "train",
+    ):
+        """Compute the DPO loss and other metrics for the given batch of inputs for train or test."""
+        metrics = {}
+
+        forward_output = self.concatenated_forward(model, batch)
+        (
+            policy_chosen_logps,
+            policy_rejected_logps,
+            policy_chosen_logits,
+            policy_rejected_logits,
+            policy_nll_loss,
+        ) = forward_output[:5]
+        if self.aux_loss_enabled:
+            aux_loss = forward_output[5]
+
+        # if reference_chosen_logps and reference_rejected_logps in batch use them, otherwise use the reference model
+        if (
+            "reference_chosen_logps" in batch
+            and "reference_rejected_logps" in batch
+            and self.args.rpo_alpha is not None
+        ):
+            reference_chosen_logps = batch["reference_chosen_logps"]
+            reference_rejected_logps = batch["reference_rejected_logps"]
+        else:
+            with torch.no_grad():
+                if self.ref_model is None:
+                    with self.null_ref_context():
+                        (
+                            reference_chosen_logps,
+                            reference_rejected_logps,
+                            _,
+                            _,
+                            _,
+                        ) = self.concatenated_forward(self.model, batch)
+                else:
+                    (
+                        reference_chosen_logps,
+                        reference_rejected_logps,
+                        _,
+                        _,
+                        _,
+                    ) = self.concatenated_forward(self.ref_model, batch)
+
+        losses, chosen_rewards, rejected_rewards = self.dpo_loss(
+            policy_chosen_logps,
+            policy_rejected_logps,
+            reference_chosen_logps,
+            reference_rejected_logps,
+            batch
+        )
+    
+        ### End of changed code
+        
+        reward_accuracies = (chosen_rewards > rejected_rewards).float()
+
+        if self.args.rpo_alpha is not None:
+            losses = losses * self.args.rpo_alpha + policy_nll_loss
+
+        prefix = "eval_" if train_eval == "eval" else ""
+        metrics[f"{prefix}rewards/chosen"] = chosen_rewards.mean().cpu()
+        metrics[f"{prefix}rewards/rejected"] = rejected_rewards.mean().cpu()
+        metrics[f"{prefix}rewards/accuracies"] = reward_accuracies.mean().cpu()
+        metrics[f"{prefix}rewards/margins"] = (chosen_rewards - rejected_rewards).mean().cpu()
+        metrics[f"{prefix}logps/rejected"] = policy_rejected_logps.detach().mean().cpu()
+        metrics[f"{prefix}logps/chosen"] = policy_chosen_logps.detach().mean().cpu()
+        metrics[f"{prefix}logits/rejected"] = policy_rejected_logits.detach().mean().cpu()
+        metrics[f"{prefix}logits/chosen"] = policy_chosen_logits.detach().mean().cpu()
+        if self.args.rpo_alpha is not None:
+            metrics[f"{prefix}nll_loss"] = policy_nll_loss.detach().mean().cpu()
+
+        if self.aux_loss_enabled:
+            return losses.mean() + getattr(model.config, "router_aux_loss_coef", 0.0) * aux_loss, metrics
+
+        return losses.mean(), metrics
+
+if __name__=="__main__":
+    dpo_trainer.DPOTrainer.get_batch_loss_metrics = margin_loss_metrics
+    dpo_trainer.DPOTrainer.dpo_loss = margin_dpo_loss
+
+    # TODO load in a dataset of a specific format (make sure that it has margins)
+    dataset = load_dataset("")
+    
+    # load model and dataset - dataset needs to be in a specific format
+    model = AutoModelForCausalLM.from_pretrained("gpt2")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+    # load trainer
+    trainer = dpo_trainer.DPOTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        train_dataset=dataset,
+    )
+
+    # train
+    trainer.train()
+    
+    
